e8fe2cc08a39dd4d343fdc7ea380893ef42740cb
Ariana <pilarsv@outlook.com>
2022-03-30 13:22:42 -0600

    Desarollo logico de  hangman.js terminado

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,35 +1,73 @@
 class Hangman {
   constructor(words) {
-    this.words = words;
-    // ... your code goes here
+    this.words = words    
+    this.secretWord = this.pickWord()
+    this.letters = []
+    this.guessedLetters = ""
+    this.errorsLeft = 10
   }
 
   pickWord() {
-    // ... your code goes here
+   // OBTENER NÚMERO ALEATORIO PARA SABER LA POSICIÓN
+  const randomNumber = Math.random() * this.words.length
+  
+  // QUITAR DECIMALES
+  const positionArr = Math.floor(randomNumber)
+  
+  // RETORNAR PALABRA ALEATORIA
+  return this.words[positionArr]
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if(keyCode >= 65 && keyCode <= 90) {
+      return true
+    } 
+       
+    return false   
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    // SI LA LETRA YA SE PRESIONÓ, ENTONCES, DEVUELVE UN FALSE
+    if(this.letters.includes(letter)){
+      return false
+       }
+  
+  // SI NO SE HA PRESIONADO, ENTONCES, DEVUELVE UN TRUE
+      return true
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+    return this.guessedLetters += letter
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    this.errorsLeft -= 1
+    if(!this.letters.includes(letter)) {
+      this.letters.push(letter)
+    }
+    return
   }
 
   checkGameOver() {
     // ... your code goes here
+    if(this.errorsLeft > 0){
+      return false
+    }
+    return true
   }
 
   checkWinner() {
     // ... your code goes here
+  for(let i=0; i<this.secretWord.length; i++){
+    if(this.guessedLetters.indexOf(this.secretWord[i])===-1){
+      return false
+    }
+  }
+  return true
   }
 }
 
